033eb5df82b050ecbbf1987e8d6f44e7f17f02a9
Constanza-Alonso <constanzaalonso04@gmail.com>
2025-04-23 16:52:53 -0700

    Add files via upload

---
+++ Restaurante/restaurante.js
@@ -0,0 +1,25 @@
+// restaurante.js
+
+document.addEventListener('DOMContentLoaded', () => {
+    // Navegación
+    document.getElementById('link-home').onclick = () => window.location.href = '../Home/home.html';
+    document.getElementById('link-vinos').onclick = () => window.location.href = '../Vinos/vinos.html';
+    document.getElementById('link-enoturismo').onclick = () => window.location.href = '../Enoturismo/enoturismo.html';
+    document.getElementById('link-ubicacion').onclick = () => window.location.href = '../Ubicacion/ubicacion.html';
+    document.getElementById('link-contacto').onclick = () => window.location.href = '../Contacto/contacto.html';
+  
+    // Íconos
+    document.getElementById('icon-usuario').onclick = () => window.location.href = '../InicioSesion/iniciosesion.html';
+    document.getElementById('icon-buscar').onclick = () => window.location.href = '../Busqueda/busqueda.html';
+    document.getElementById('icon-carrito').onclick = () => window.location.href = '../Detalle de producto/compra.html';
+  
+    // Botones (si están en tu HTML)
+    const btnEnoturismo = document.getElementById('btn-enoturismo');
+    const btnVinos = document.getElementById('btn-vinos');
+    const btnExperiencias = document.getElementById('btn-experiencias');
+  
+    if (btnEnoturismo) btnEnoturismo.onclick = () => window.location.href = '../Enoturismo/enoturismo.html';
+    if (btnVinos) btnVinos.onclick = () => window.location.href = '../Vinos/vinos.html';
+    if (btnExperiencias) btnExperiencias.onclick = () => window.location.href = '../Restaurante/restaurante.html';
+  });
+  
