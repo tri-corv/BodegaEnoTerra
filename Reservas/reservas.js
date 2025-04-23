31ecbed35231957f31ecdb2901c8bafa6dc577a0
Constanza-Alonso <constanzaalonso04@gmail.com>
2025-04-23 16:53:29 -0700

    Add files via upload

---
+++ Reservas/reservas.js
@@ -0,0 +1,15 @@
+// Funciones del menú
+document.getElementById('link-home').onclick = () => window.location.href = '../Home/home.html';
+document.getElementById('link-vinos').onclick = () => window.location.href = '../Vinos/vinos.html';
+document.getElementById('link-enoturismo').onclick = () => window.location.href = '../Enoturismo/enoturismo.html';
+document.getElementById('link-ubicacion').onclick = () => window.location.href = '../Ubicacion/ubicacion.html';
+document.getElementById('link-contacto').onclick = () => window.location.href = '../Contacto/contacto.html';
+
+// Íconos
+document.getElementById('icon-usuario').onclick = () => window.location.href = '../InicioSesion/iniciosesion.html';
+document.getElementById('icon-buscar').onclick = () => window.location.href = '../Busqueda/busqueda.html';
+document.getElementById('icon-carrito').onclick = () => window.location.href = '../DetalleProducto/compra.html';
+
+// Botones de reserva
+document.getElementById('btn-degustacion').onclick = () => window.location.href = '../Reservas/degustacion.html';
+document.getElementById('btn-almuerzo').onclick = () => window.location.href = '../Reservas/almuerzo.html';
