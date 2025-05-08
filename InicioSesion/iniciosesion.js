cc10b20b64e56df5d7050a60705e19b88d10bac8
tri-corv <trinidadcrvln@gmail.com>
2025-05-07 21:27:42 -0300

    Validacion de Inicio Sesion
    Validaciones de datos (mail, contraseña) en el inicio de sesión

--- InicioSesion/iniciosesion.js
+++ InicioSesion/iniciosesion.js
@@ -12,8 +12,20 @@ document.getElementById('icon-carrito').onclick = () => window.location.href = '
 
 //Boton "Iniciar Sesión"
 document.getElementById('btn-iniciar-sesion').onclick = (e) => {
-    e.preventDefault(); // Evita que se recargue la página
-    window.location.href = '../Home/home.html'; 
+    e.preventDefault();
+    
+    const formulario = document.querySelector('form');
+
+    if (formulario.checkValidity()) {
+        // Redirigir después del mensaje
+        window.location.href = '../Home/home.html'; 
+
+        // Mostrar mensaje de sesión iniciada
+        alert("Se ha iniciado sesión correctamente");
+    } else {
+        // Mostrar validación nativa del navegador
+        formulario.reportValidity();
+    }
 };
 
 //Link Registro
@@ -21,3 +33,19 @@ document.getElementById('link-registro').onclick = (e) => {
     e.preventDefault(); // Evita que el enlace haga scroll arriba
     window.location.href = '../Registro/registro.html'; 
 };
+
+//Contrasena
+const inputPassword = document.getElementById("contrasena");
+const togglePassword = document.getElementById("toggle-password");
+
+togglePassword.onclick = () => {
+  const tipoActual = inputPassword.getAttribute("type");
+  if (tipoActual === "password") {
+    inputPassword.setAttribute("type", "text");
+    togglePassword.textContent = "🙈"; // Cambia el ícono cuando se muestra
+  } else {
+    inputPassword.setAttribute("type", "password");
+    togglePassword.textContent = "👁️"; // Vuelve al ícono original
+  }
+};
+
